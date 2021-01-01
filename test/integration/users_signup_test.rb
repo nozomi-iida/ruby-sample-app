9b9ad33183ab958ec6d7c364fa813e1cf3dbfa88
nozomi-iida <iida19990106@gmail.com>
2021-01-01 18:24:51 +0900

    fix: test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -15,10 +15,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "valid signup information" do
     get signup_path
     assert_difference "User.count", 1 do
-      post u sers_path, params: { user: { name: "user",
-                                         email: "user@test.com",
-                                         password: "111111",
-                                         password_confirmation: "111111" } }
+      post users_path, params: { user: { name: "user",
+                                        email: "user@test.com",
+                                        password: "111111",
+                                        password_confirmation: "111111" } }
     end
     follow_redirect!
     assert_template "users/show"
